aef7d9382b7a97054ed09951d4fbc5800ba9ff03
kailash Maurya <kailashmaurya51@gmail.com>
2025-01-26 11:46:49 +0530

    initial commmit

--- src/components/Achievement.jsx
+++
@@ -1,9 +0,0 @@
-const Achievement = () => {
-    return (
-        <div name="achievement" className='py-5 bg-gradient-to-b from-gray-800 to-black w-full h-auto text-white md:h-screen'>
-            {/* ... existing code */}
-        </div>
-    );
-}
-
-export default Achievement;
